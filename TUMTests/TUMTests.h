d868029f55051d06655013d605d9627819d342d7
Alejandro Cruz Paz <vidriloco@gmail.com>
2012-04-20 12:10:52 -0500

    Initial Commit

---
+++ TUMTests/TUMTests.h
@@ -0,0 +1,13 @@
+//
+//  TUMTests.h
+//  TUMTests
+//
+//  Created by Alejandro on 20/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TUMTests : SenTestCase
+
+@end
---
+++ TUMTests/TUMTests.m
@@ -0,0 +1,32 @@
+//
+//  TUMTests.m
+//  TUMTests
+//
+//  Created by Alejandro on 20/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TUMTests.h"
+
+@implementation TUMTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TUMTests");
+}
+
+@end
